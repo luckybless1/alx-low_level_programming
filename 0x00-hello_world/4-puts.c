a8333c5be786730376babef98b8c7680e76c86a0
lucky bless <luckybless2014@gmail.com>
2022-09-08 15:21:00 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-
 #include<stdio.h>
 /**
  *  * main -  prints exactly "Programming is like building a multilingual puzzle.
@@ -9,6 +8,6 @@ int main(void)
 {
 	
 
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
