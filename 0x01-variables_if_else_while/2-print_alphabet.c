39bc2c8acd8632d1529576e9c6b6d56e0cc1eee7
lucky bless <luckybless2014@gmail.com>
2022-09-12 13:03:12 -0700

    The last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main prints the alphabet in lowercase,followed by a new line
+ *
+ * Return: Always 0 (success)
+ **/
+
+int main(void)
+{
+	char c;
+
+	for (c= 'a';c <= 'z';c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
