6e6dbd9d0d970a90f1d860741f7fcca614d6b837
lucky bless <luckybless2014@gmail.com>
2022-09-12 06:24:03 -0700

    positive and negative ,zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ int main(void)
 	
 	if (n > 0)
 	{
-		printf("%d you have a positive number \n" , n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d you have a zero number \n" , n);
+		printf("%d is zero\n", n);
 	}
 	else 
 	{
-		printf("%d you have a negative number \n" , n);
+		printf("%d is negative\n", n);
 	}
 	
 	return (0);
