3a49f55984a7a1c04332b707ae07d316420398cc
lucky bless <luckybless2014@gmail.com>
2022-09-12 17:39:59 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
  * main -  prints exactly Programming is like building a multilingual puzzle.
- *   
- *   Return: Always 0 (Success)
+ *
+ * Return: Always 0 (Success)
  **/
 
 int main(void)
