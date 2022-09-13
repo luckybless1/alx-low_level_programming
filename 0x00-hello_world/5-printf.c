a8d4c09c2400547ba3e207d007806481edbb5e4c
lucky bless <luckybless2014@gmail.com>
2022-09-12 17:52:21 -0700

    hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-int main(void)
 /**
  * main - prints with proper grammer, but the outcome is a piece of art,
  *
- * Return:Always 0 (success)
+ * Return: Always 0 (success)
  **/
-	
+int main(void)	
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
