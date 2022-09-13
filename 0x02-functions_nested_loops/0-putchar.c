575cdedb590070d3703d4d892232fe310f13286e
lucky bless <luckybless2014@gmail.com>
2022-09-13 09:29:03 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints _putchar,folloed by a new line
+ *
+ * Return:Always 0 (success)
+ **/
+int main(void)
+{
+	printf("_putchar");
+	prinf('\n');
+
+	return (0);
+}
