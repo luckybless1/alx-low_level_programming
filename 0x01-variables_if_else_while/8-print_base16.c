fbde74d0aab23ce071991ded9b6b86df73ab13dc
lucky bless <luckybless2014@gmail.com>
2022-09-12 19:08:43 -0700

    print base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	char i;
 
-	for (i ='0'; i <= '9'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
 	}
@@ -18,6 +18,5 @@ int main(void)
 		putchar(i);
 	}
 	putchar('\n');
-	
 	return (0);
 }
