dc403d96950363768c246af87b20d0b6703db3c9
lucky bless <luckybless2014@gmail.com>
2022-09-14 11:03:11 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign og a number
+ * @n: input
+ * Return: 1 if n is positive, o if n is zero and -1 otherwise
+ **/
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+		_putchar('+);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-1');
+		return (-1);
+	}
+}
