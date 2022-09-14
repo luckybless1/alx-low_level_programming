8834382e9f2e343f0eb9ac4517008924fe662d97
lucky bless <luckybless2014@gmail.com>
2022-09-14 14:02:06 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -21,17 +21,19 @@ void print_to_98(int n)
 		}
 		else if (n > 98)
 		{
-			printf("%d", n);
-			if (n != 98)
+			while (n >= 98)
 			{
+				printf("%d", n);
+				if (n != 98)
+				{
 				printf(", ");
+				}
+				n--;
 			}
-			n--;
 		}
-	}
-	else
-	{
-		printf("98");
-	}
-	printf('\n');
+		else
+		{
+			printf("98");
+		}
+		printf('\n');
 }
