4ed6c1a50fc8debe75194e7a846420494bb3f78e
lucky bless <luckybless2014@gmail.com>
2022-09-14 13:20:33 -0700

    add a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add two numbers and return the result
+ * @a: first number input
+ * @b: second number input
+ * Return: a+b
+ **/
+int add(int a, int b)
+{
+
+	return (a + b);
+}
