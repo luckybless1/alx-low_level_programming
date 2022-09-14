ab4758ad40ebacfc596550a784e944366d9e0821
lucky bless <luckybless2014@gmail.com>
2022-09-14 13:46:46 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * print_to_98 - a function that prints all numbers form n to 98
+ * @n:number input
+ * return: Always 0 (success)
+ **/
+void print_to_98(int n)
+{
+
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf"%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+		else if (n > 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	else
+	{
+		printf("98");
+	}
+	printf('\n');
+}
