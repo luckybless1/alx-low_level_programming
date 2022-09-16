2e4bb0a4712c2b9ecba002e20cf9b132c0c6924e
lucky bless <luckybless2014@gmail.com>
2022-09-15 23:57:11 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _isdigit - check for a digit through 0 to 9
+ * _isdigiit - check for a digit through 0 to 9
  * @c: input that is going to check
  * Return: 1 if @c is a digiy , otherwise 0
  **/
 int _isdigit(int c)
 {
 
-	if (c >= 0 && c <=  9)
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
