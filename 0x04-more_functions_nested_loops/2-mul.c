a012dc0c3e1a6b583a2177ef6b34153b3f0657e1
lucky bless <luckybless2014@gmail.com>
2022-09-16 09:02:12 -0700

    multipy

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *  * main - check the code
+ *@a: first input
+ *@b: second input
+ * Return: multiplication result  of 2 inputs
+ **/
+int mul(int a, int b)
+{
+
+	return (a * b);
+}
