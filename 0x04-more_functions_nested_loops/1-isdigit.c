81ee3096ebce8a40a7da08009cf17a2e8967f55e
lucky bless <luckybless2014@gmail.com>
2022-09-16 09:09:18 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 /**
  * _isdigiit - a function that  checks for a digit to 0-9
  * @c: input that is going to check
- * Return: 1 if @c is a digiy , otherwise 0
+ * Return: 1 if @c is a digit , otherwise 0
  **/
 int _isdigit(int c)
 {
