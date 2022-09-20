5dd04416c8004e3cc3bf4d64a17ca3209305f3a0
lucky bless <luckybless2014@gmail.com>
2022-09-19 17:56:48 -0700

    update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - the function updates the value of the pointer to 98
+ * @n: pointer reset to 98
+ * Return: void
+ **/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
